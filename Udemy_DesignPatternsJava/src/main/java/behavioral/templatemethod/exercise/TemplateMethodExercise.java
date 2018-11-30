865b89b8fb334e8fdd6a7f80b8c8d817476fa3b7
ciro <cirux88@gmail.com>
2018-11-30 09:04:21 +0100

    Template Method

--- Udemy_DesignPatternsJava/src/main/java/behavioral/templatemethod/exercise/TemplateMethodExercise.java
+++ Udemy_DesignPatternsJava/src/main/java/behavioral/templatemethod/exercise/TemplateMethodExercise.java
@@ -9,6 +9,11 @@ class Creature {
 	}
 }
 
+/**
+ * This abstract class is the template method: the algoritm is defined at high
+ * level
+ *
+ */
 abstract class CardGame {
 	public Creature[] creatures;
 
@@ -22,41 +27,52 @@ abstract class CardGame {
 		Creature second = creatures[creature2];
 		hit(first, second);
 		hit(second, first);
-		// todo: determine who won and return either creature1 or creature2
-		return creature2;
+		// determine who won and return either creature1 or creature2
+		boolean creature1Alive = first.health > 0;
+		boolean creature2Alive = second.health > 0;
+		if (creature1Alive == creature2Alive)
+			return -1; // empasse
+		return creature1Alive ? creature1 : creature2;
 	}
 
 	// attacker hits other creature
 	protected abstract void hit(Creature attacker, Creature other);
 }
 
+/**
+ * Unless the Creature has been killed, its health returns to the original value
+ * at the end of combat
+ *
+ */
 class TemporaryCardDamageGame extends CardGame {
 
 	public TemporaryCardDamageGame(Creature[] creatures) {
 		super(creatures);
-		// TODO Auto-generated constructor stub
 	}
-	// todo
 
 	@Override
 	protected void hit(Creature attacker, Creature other) {
-		// TODO Auto-generated method stub
-		
+		int oldHotherHealth = other.health;
+		other.health -= attacker.attack;
+		if (other.health > 0)
+			other.health = oldHotherHealth; // restore
+
 	}
 }
 
+/**
+ * health damage persists
+ *
+ */
 class PermanentCardDamageGame extends CardGame {
 
 	public PermanentCardDamageGame(Creature[] creatures) {
 		super(creatures);
-		// TODO Auto-generated constructor stub
 	}
-	// todo
 
 	@Override
 	protected void hit(Creature attacker, Creature other) {
-		// TODO Auto-generated method stub
-		
+		other.health -= attacker.attack;
 	}
 }
 
